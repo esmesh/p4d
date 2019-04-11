827945c62b628e441d77cfcc9be02c4bb54bc1a7
Erika Mesh <esmvcs@ad.rit.edu>
2019-04-11 10:24:20 -0400

    Turned array of snowflakes into confetti and made spinning snowflakes

--- SpinningSnowflakes/snowflake.js
+++ SpinningSnowflakes/snowflake.js
@@ -8,7 +8,7 @@ class Snowflake
         this.y = random(height);
         this.size = random(10,20);
         this.speed = random(1,3);
-        this.flakeColor = color(random(255), random(255), random(255));
+        this.rotation = 0;
     }
 
     move()
@@ -18,15 +18,29 @@ class Snowflake
         
         // Wrap the snowflake back to the top of the canvas
         this.y %= height; // Divides y by the height and then sets y to the remainder
+        
+        // Rotate a bit
+        this.rotation += 2;
+        if(this.rotation >= 360)
+            this.rotation = 0;
     }
     
     display()
     {
         push();
-        // Make our snowflakes blue
-        fill(this.flakeColor);
-        noStroke();
-        ellipse(this.x, this.y, this.size, this.size);
+        angleMode(DEGREES);
+        stroke(255, 255, 255, 75);
+        
+        translate(this.x, this.y);
+        rotate(this.rotation);
+        line(0, 0, -this.size, 0);
+        line(0, 0, -this.size, -this.size);
+        line(0, 0, 0, -this.size);
+        line(0, 0, this.size, -this.size);
+        line(0, 0, this.size, 0);
+        line(0, 0, this.size, this.size);
+        line(0, 0, 0, this.size);
+        
         pop();
     }
 }
